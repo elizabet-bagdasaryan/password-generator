4ec38626b52c426033f598a669b6dbaa51c79559
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-04-13 21:58:36 +0400

    "feat:added checkboxes strength outputting logic"

--- src/Strength.jsx
+++ src/Strength.jsx
@@ -8,7 +8,30 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import "./Strength.css";
 
 function Strength() {
-   
+    const [checkedBoxes, setCheckedBoxes] = useState([]);
+
+    const handleCheckboxChange = (event) => {
+        const { value, checked } = event.target;
+        if (checked) {
+          setCheckedBoxes([...checkedBoxes, value]);
+        } else {
+          setCheckedBoxes(checkedBoxes.filter((item) => item !== value));
+        }
+      };
+
+      const getImage = () => {
+        const numChecked = checkedBoxes.length;
+        if (numChecked === 1) {
+          return <img src={TooWeak} />;
+        } else if (numChecked === 2) {
+          return <img src={Weak} />;
+        } else if (numChecked === 3) {
+          return <img src={Medium} />;
+        } else if (numChecked === 4) {
+            return <img src={Strong} />;
+          } 
+      }
+
 
 
 
@@ -24,7 +47,7 @@ function Strength() {
             id="uppercase"
             name="uppercase"
             value="upper"
-        
+            onChange={handleCheckboxChange}
           />
           <span class="checkmark"></span>
         </div>
@@ -35,23 +58,23 @@ function Strength() {
             id="lowercase"
             name="lowercase"
             value="lower"
-          
+            onChange={handleCheckboxChange}
           />
           <span class="checkmark"></span>
         </div>
         <div class="container">
           <label for="numbers">Include Numbers</label>
-          <input type="checkbox" id="numbers" name="numbers" value="nums"   />
+          <input type="checkbox" id="numbers" name="numbers" value="nums"  onChange={handleCheckboxChange} />
           <span class="checkmark"></span>
         </div>
         <div class="container">
           <label for="symbols">Include Symbols</label>
-          <input type="checkbox" id="symbols" name="symbols" value="sym"   />
+          <input type="checkbox" id="symbols" name="symbols" value="sym"  onChange={handleCheckboxChange} />
           <span class="checkmark"></span>
         </div>
       </form>
       <div className="img-container">
-       
+      {getImage()}
       </div>
       <button>
         <p>GENERATE </p> <ArrowForwardIcon className="arrow" />
