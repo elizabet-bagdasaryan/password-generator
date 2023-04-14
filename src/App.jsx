b628ab4a7d4b6121592fc24310e8ab6644176723
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-04-14 17:40:39 +0400

    "feat: added password generating"

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from "react";
 
 import "./App.css";
 
-import Password from "./Password";
-import Scrollbar from "./Scrollbar";
+
 import Strength from "./Strength";
 
 function App() {
   return (
     <div className="App">
       <p className="generator-text">Password Generator</p>
-      <Password />
+    
       <div className="func-section">
-        <Scrollbar />
+      
         <Strength />
       </div>
     </div>
